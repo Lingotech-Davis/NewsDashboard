7ee3278bd4f8c0e8c9184d6cf21682ceabae0ce8
ALPilapil <alpilapil@ucdavis.edu>
2025-07-20 01:06:12 -0700

    adding env variables

--- tests/api_test.py
+++ tests/api_test.py
@@ -1,10 +1,13 @@
 from google import genai
 from google.genai import types
+import os
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 def ask_gemini(prompt, content, test_mode=False):
     if test_mode:
         return "In test mode, will not call API"
-    client = genai.Client(api_key="")
+    client = genai.Client(api_key=GEMINI_API_KEY)
 
     contents = prompt + content
 
